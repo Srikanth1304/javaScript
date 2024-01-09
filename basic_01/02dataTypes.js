685eae8aedd6b1756c6c2d52f0af496ba2591f96
Git_Srikanth <srikanthchebrolu727@gmail.com>
2024-01-09 23:08:34 +0530

    datatypes

---
+++ basic_01/02dataTypes.js
@@ -0,0 +1,23 @@
+"strict use";
+let age=18;
+let name='Srikanth'
+let isKind=true;
+let weight;
+console.log(typeof age)
+
+
+//Datatypes
+/*
+number=>2 power of 53
+bigint 
+String=>" "
+boolean=> t/f
+null
+undefined
+
+object 
+
+
+typeof null    =>object
+typeof undefined =>undefined
+*/
