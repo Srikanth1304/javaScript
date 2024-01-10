ac8f77f27f39a015e6195b9bb97ee79d48e7c635
Git_Srikanth <srikanthchebrolu727@gmail.com>
2024-01-10 19:32:37 +0530

    operations

---
+++ basic_01/04operations.js
@@ -0,0 +1,19 @@
+// let x=4
+// console.log(x+4)
+
+// let s1='Srikanth'
+// let s2=' An****a'
+// let s3=s1+ s2
+// console.log(s3)
+
+    //-------------ECMAScript--------------
+// console.log(('4'+3))==>43 String
+// console.log(4+'3')==>43 String
+
+// console.log('2'+3+4) =>243 String
+// console.log(3+4+'5')  =>75 String
+
+    //------------MDN------------
+//Unary postfix: Return the valur after the increment
+//      prefix: Return the value before the increment 
+
---
+++ basic_01/comparision.js
@@ -0,0 +1,31 @@
+// console.log(2>3)
+// console.log(2>=3)
+// console.log(2==3)
+// console.log(2<3)
+// console.log(2<=3)
+// console.log(2!=3)
+
+         //Will run fine
+// console.log('2'<3)
+// console.log(2<'3')
+// console.log('2'<'3')
+// console.log('2'==3)
+// console.log('2'!=3)
+
+        //=== compares mainly datatypes 
+
+        // when null in context, equality check is differently from camparision , 
+        //comparision takes null as zero 
+// console.log(null>0)
+// console.log(null>=0)
+// console.log(null<=0)
+// console.log(null==0)
+
+// console.log(undefined>0)
+// console.log(undefined<0)
+// console.log(undefined>=0)
+// console.log(undefined<=0)
+// console.log(undefined==0)
+// console.log(undefined!=0)
+
+
