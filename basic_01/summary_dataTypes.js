93ee2358fb0cf4b8aded857f5db309e863d349c1
Git_Srikanth <srikanthchebrolu727@gmail.com>
2024-01-11 22:00:53 +0530

    summary

---
+++ basic_01/summary_dataTypes.js
@@ -0,0 +1,4 @@
+//primitive datatype: 7 : (StaCk) : Copy :changes made, copy willl change ,original remains same 
+     //Number,BigInt, String, Boolean, Symbol,null,undefined
+//Non premitive or reference:(Heap):=> changes made ,original will change
+     //Array, Object, function
