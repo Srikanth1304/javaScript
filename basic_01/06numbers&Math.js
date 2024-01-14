ed5df92124bdfa7b375f1cf7d25541693fbb3cf1
Git_Srikanth <srikanthchebrolu727@gmail.com>
2024-01-14 18:39:57 +0530

    math

---
+++ basic_01/06numbers&Math.js
@@ -0,0 +1,39 @@
+// const num=37
+// console.log(num)
+
+// const jntu=new Number(537)
+// console.log(jntu)
+// console.log(jntu.toFixed(2))
+// console.log(jntu.toString())
+
+// const cash=1000000
+// console.log(cash.toLocaleString())
+// console.log(cash.toLocaleString('en-US'))
+
+// const pre=537.22
+// console.log(pre)
+// console.log(pre.toExponential())
+// console.log(pre.toExponential(1)) //after decimal
+// console.log(pre.toExponential(2))
+// console.log(pre.toExponential(3))
+// console.log(pre.toExponential(4))
+// console.log(pre.toExponential(5))
+// console.log(pre.toExponential(6))
+// console.log()
+
+// console.log(pre.toPrecision()) //before decimal
+// console.log(pre.toPrecision(1))
+// console.log(pre.toPrecision(2))
+// console.log(pre.toPrecision(3))
+// console.log(pre.toPrecision(4))
+// console.log(pre.toPrecision(5))
+// console.log(pre.toPrecision(6))
+
+
+/////////////++++++MAth++++++////////////
+//math is an object that has many methods
+//random always generates b/w 0 1
+const max=20
+const min=10
+console.log(Math.floor(Math.random(max-min+1)*10+min))
+
