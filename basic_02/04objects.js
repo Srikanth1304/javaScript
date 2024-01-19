90dea1e7c26dee5bece9791a16208e58ae861663
Git_Srikanth <srikanthchebrolu727@gmail.com>
2024-01-19 16:34:31 +0530

    destructuringg

--- basic_02/04objects.js
+++ basic_02/04objects.js
@@ -8,3 +8,30 @@ console.log(obj3)
 
 console.log(Object.keys(obj1))
 console.log(obj1.hasOwnProperty('d'))
+
+//obj destructuing 
+const course={
+    courseName:'JS',
+    duration:'12weeks',
+    courseInstructor:'Srikanth'
+
+}
+//console.log(course.courseInstructor)     it is too hard to write such hard line 
+const {courseInstructor:Ins}=course
+console.log(Ins)
+
+
+
+//++++++++++++++++++++++JSON
+{
+    "name": "Srikanth",
+    'age': 21,
+
+
+}
+
+or 
+
+[
+    {},{},{}  //in case of many 
+]
